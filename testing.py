23bf8dcdecebbb49ad729709ce6056250feb8ffb
M1Al3x <90932420+M1Al3x@users.noreply.github.com>
2023-09-17 20:31:39 -0600

    Edit testing.py

--- testing.py
+++ testing.py
@@ -1,4 +1,3 @@
 import requests
-print(requests.__version__)
-res = requests.get("http://google.com")
-print(res)
+res = requests.get("https://raw.githubusercontent.com/M1Al3x/404_lab0/master/testing.py", )
+print(res.text)
